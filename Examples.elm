010595560d546b85d43bcdf5bc242af5ee442160
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2022-12-21 15:50:34 -0500

    Create Examples.elm

---
+++ Examples.elm
@@ -0,0 +1,16 @@
+pb = PocketBase.init "http://127.0.0.0.1:8090" -- or elm URL
+result = 
+    pb 
+    |> PocketBase.collections "example" 
+    |> PocketBase.getList 1 20 
+        { filter = "status = true && created > '2022-08-01 10:00:00'"}
+
+userData = 
+    pb 
+    |> PocketBase.collections "users"
+    |> PocketBase.authWithPassword "test@example.com" "123456"
+
+adminData = 
+    pb 
+    |> PocketBase.admins
+    |> PocketBase.authWithPassword "test@example.com" "123456"
